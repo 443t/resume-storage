640518026a8b89ce33b513027b73145f87b99180
Andrey Levin <andrey.lvn@gmail.com>
2017-02-22 10:44:47 +0300

    Realise methods (clear, save, delete, get, size)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,59 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int curr = 0;
 
     void clear() {
+        storage = new Resume[10000];
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                return;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume m = null;
+        for(Resume r: storage)  {
+            if (r != null && r.toString().equals(uuid)) {
+                m = r;
+            }
+        }
+        return m;
     }
 
     void delete(String uuid) {
+
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].toString().equals(uuid)) {
+                storage[i] = null;
+
+
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+
+        Resume[] tmp = new Resume[10000];
+        return tmp = Arrays.stream(storage)
+                .filter(s -> (s != null))
+                .toArray(Resume[]::new);
+
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
