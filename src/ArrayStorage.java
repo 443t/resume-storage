f88f9380f713809dcc32a8e4a4e6d25ae326af12
Andrey Levin <andrey.lvn@gmail.com>
2017-02-27 22:44:30 +0300

    Added check for unique input and Overflow check

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,44 +1,71 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Arrays.stream;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int curr = 0;
+    protected static final int STORAGE_LIMIT = 10000;
+    Resume[] storage = new Resume[STORAGE_LIMIT];
+    int size = 0;
+
 
     void clear() {
-        storage = new Resume[10000];
+        storage = new Resume[STORAGE_LIMIT];
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                return;
+
+        if (get(r.toString()) != null) {
+            System.out.println("Sorry, it can't be save because Resume with uuid = " + r.toString() + " already exist!");
+        } else if (isOverflow()) {
+            System.out.println("Sorry, no more space in Array");
+        } else {
+
+            {
+                storage[size++] = r;
+
             }
         }
     }
 
+
     Resume get(String uuid) {
         Resume m = null;
-        for(Resume r: storage)  {
+        for (Resume r : storage) {
             if (r != null && r.toString().equals(uuid)) {
                 m = r;
+                return m;
             }
         }
-        return m;
+        return null;
+    }
+
+    Resume[] clean(Resume[] v) {
+        List<Resume> list = new ArrayList<Resume>(Arrays.asList(v));
+        list.removeAll(Collections.singleton(null));
+        return list.toArray(new Resume[list.size()]);
     }
 
     void delete(String uuid) {
 
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i] != null && storage[i].toString().equals(uuid)) {
                 storage[i] = null;
 
+                clean(storage);
+                size--;
+
+                System.out.print(size());
 
             }
         }
+
+
     }
 
     /**
@@ -46,14 +73,22 @@ public class ArrayStorage {
      */
     Resume[] getAll() {
 
-        Resume[] tmp = new Resume[10000];
-        return tmp = Arrays.stream(storage)
+        Resume[] tmp = new Resume[STORAGE_LIMIT];
+        return tmp = stream(storage)
                 .filter(s -> (s != null))
                 .toArray(Resume[]::new);
 
     }
 
+    protected boolean isOverflow() {
+        if (size >= (STORAGE_LIMIT - 1))
+            return true;
+        else
+            return false;
+    }
+
+
     int size() {
-        return storage.length;
+        return size;
     }
 }
